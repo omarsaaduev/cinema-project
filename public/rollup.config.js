07a6184221b7ace4a901473c358ef6c0b227bce3
Omar Saaduev <saaduevomar05@gmail.com>
2024-03-09 18:31:20 +0300

    rollup

---
+++ public/rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+  ],
+  external: ['react', 'react-dom', 'react-redux'],
+};
